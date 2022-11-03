bb33a218f9531e65a40524e69423fb2870085208
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-11-03 18:13:25 +0700

    Tao moi mot lien he

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,43 +1,37 @@
 <template>
-    <h4>Thêm liên hệ</h4>
-    <v-from>
-        <div class="form-group">
-            <label for="name">Tên</label>
-            <input name="name" type="text" class="form-control"/>
-            <ErrorMessage name="name" class="error-feedback" />
-        </div>
-        <div class="form-group">
-            <label for="email">E-mail</label>
-            <input name="email" type="email" class="form-control"/>
-            <ErrorMessage name="email" class="error-feedback" />
-        </div>
-        <div class="form-group">
-            <label for="address">Địa chỉ</label>
-            <input name="address" type="text" class="form-control"/>
-            <ErrorMessage name="address" class="error-feedback" />
-        </div>
-        <div class="form-group">
-            <label for="phone">Điện thoại</label>
-            <input name="phone" type="tel" class="form-control"/>
-            <ErrorMessage name="phone" class="error-feedback" />
-        </div>
-        <div class="form-group form-check">
-            <input name="favorite" type="checkbox" class="form-check-input" />
-            <label for="favorite" class="form-check-label">
-                <strong>Liên hệ yêu thích</strong>
-            </label>
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-primary">Lưu</button>
-           
-        </div>
-
-    </v-from>
-  
+    <div class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm 
+            :contact="contact" 
+            @submit:contact="postContact"
+        />
+        <p>{{ message }}</p>
+    </div>
 </template>
-<script>
-import ContactForm from "@/components/ContactForm.vue";
-import ContactService from "@/services/contact.service";
-
-</script>
+    <script>
+     import ContactForm from "../components/ContactForm.vue";
+     import ContactService from "../services/contact.service";
+     export default {
+         components: {
+             ContactForm,        
+         },
+         data() {
+             return {
+                 contact: {},
+                 message: "",
+             };
+         },
+         methods: {
+             async postContact(data) {
+                 try {
+                     await ContactService.create(data);
+                     this.message = "Liên hệ được thêm thành công.";
+                 } catch (error) {
+                     console.log(error);
+                 }
+             },
+         },
+         created() {
+             this.message = ""; },
+     };
+    </script>
