898a2a5e7c42c1372533dd41d0874c587fc5b377
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-11-03 17:12:26 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,43 @@
+<template>
+    <h4>Thêm liên hệ</h4>
+    <v-from>
+        <div class="form-group">
+            <label for="name">Tên</label>
+            <input name="name" type="text" class="form-control"/>
+            <ErrorMessage name="name" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <input name="email" type="email" class="form-control"/>
+            <ErrorMessage name="email" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="address">Địa chỉ</label>
+            <input name="address" type="text" class="form-control"/>
+            <ErrorMessage name="address" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="phone">Điện thoại</label>
+            <input name="phone" type="tel" class="form-control"/>
+            <ErrorMessage name="phone" class="error-feedback" />
+        </div>
+        <div class="form-group form-check">
+            <input name="favorite" type="checkbox" class="form-check-input" />
+            <label for="favorite" class="form-check-label">
+                <strong>Liên hệ yêu thích</strong>
+            </label>
+        </div>
+
+        <div class="form-group">
+            <button class="btn btn-primary">Lưu</button>
+           
+        </div>
+
+    </v-from>
+  
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+</script>
